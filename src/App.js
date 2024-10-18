d4cfbade7bb41f9541c0a2d4db6a61a8552ffec8
John Moore <jmoore70@patriots.uttyler.edu>
2024-10-18 13:22:34 -0500

    Womp womp

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+        <header className="App-header">
+            <center>
+                <div id="center">
+                    test
+
+                    <textarea readOnly id="chat">
+                        bob
+                    </textarea>
+                    <textarea onSubmit={chat} autoFocus id="typeChat">
+                    </textarea>
+                </div>
+            </center>
+    </header>
+</div>
+);
+}
+
+function chat() {
+    document.querySelector('textarea#chat').innerHTML += this.innerHTML;
 }
 
 export default App;
