05b3f8bd737b8b912c2d04607b27f17ab6cb48ee
Jack <jackm2home@gmail.com>
2024-11-27 15:41:48 -0600

    Changed Timer to send events to every socket in lobby

--- backend/Lobby.js
+++ backend/Lobby.js
@@ -1,6 +1,6 @@
 import Game from "./Game.js"
 
-export default class Lobby{
+export default class Lobby {
     static lobbyList = {};
     inactivePlayers = [];
 
@@ -21,7 +21,7 @@ export default class Lobby{
         return false;
     }
 
-    kickPlayer(playerId){
+    kickPlayer(playerId) {
         if (this.playerList.includes(playerId)) {
             this.playerList = this.playerList.filter(id => id !== playerId);
             return true;
@@ -37,26 +37,25 @@ export default class Lobby{
 
 
     //sets the host of the lobby
-    setHost(player){
+    setHost(player) {
         this.host = player;
     }
 
     //starts the game (currently pseudocode)
     startGame(playerList, host) {
         //if isReady in playerlist is all true, prompt host to start game
-        if(playerList.every(player => player.isReady === true)){
+        if (playerList.every(player => player.isReady === true)) {
             let activeGame = new Game(this);
             activeGame.gameLoop();
-        }
-        else
-        console.log("Players not all ready");
+        } else
+            console.log("Players not all ready");
         //integrate with front end
     }
 
-    generateLobbyCode(){
+    generateLobbyCode() {
         let lobbyCode = Math.floor(Math.random() * (99999999 - 10000000)) + 10000000;
         //generates unique lobby code
-        while(Object.keys(Lobby.lobbyList).includes(lobbyCode)) {
+        while (Object.keys(Lobby.lobbyList).includes(lobbyCode)) {
             lobbyCode = Math.floor(Math.random() * (99999999 - 10000000)) + 10000000;
         }
         return lobbyCode;
@@ -65,32 +64,30 @@ export default class Lobby{
     //function to start the timer
     startTimer(startTimeInMinutes) {
 
-    //turns the minutes into countable seconds
-    let timeInSeconds = startTimeInMinutes * 60;
-    
-    //set the id of the paragraph in html, example is "timer"
-    const countdownElement = document.getElementById('timer');
-    
-    //a bit sloppy, but decrements the count by 1 second and stops when reaches 0
-    const interval = setInterval(function(){updateTimer(timeInSeconds,countdownElement); 
-    
-        timeInSeconds--
-        
-        if(timeInSeconds < 0){
-            clearInterval(interval);
+        //turns the minutes into countable seconds
+        let timeInSeconds = startTimeInMinutes * 60;
+
+        //a bit sloppy, but decrements the count by 1 second and stops when reaches 0
+        const interval = setInterval(() => {
+            this.updateTimer(timeInSeconds);
+            timeInSeconds--
+
+            if (timeInSeconds < 0) {
+                clearInterval(interval);
             }
         }, 1000);
     }
     //function to update the timer display
-    updateTimer(timeInSeconds, countdownElement) {
-        const minutes = Math.floor(timeInSeconds / 60);
+    updateTimer(timeInSeconds) {
+        const minutes = Math.floor(timeInSeconds / 60).toString();
         let seconds = timeInSeconds % 60;
     
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            seconds = seconds;
-    
-        countdownElement.innerHTML = `${minutes}: ${seconds}`;
+        seconds = seconds < 10 ? "0" + seconds : seconds.toString();
+
+        this.playerList.forEach(p => {
+            p.currentSocket.emit("time", minutes, seconds);
+        })
+        // countdownElement.innerHTML = `${minutes}: ${seconds}`;
     }
 
     close() {
